a569667e8f6235423d0d3f5d660accf7947138cf
klsruan <rdkleriston@gmail.com>
2023-03-27 17:28:08 -0300

    Add ILL.IMG

--- ffi-packages/build.bat
+++ ffi-packages/build.bat
@@ -1,8 +1,17 @@
 @echo off
 
-if not exist ffi-packages\build ( mkdir ffi-packages\build )
+@rem CLipper2
+if not exist ffi-packages\Clipper2\buildCP ( mkdir ffi-packages\Clipper2\buildCP )
 
 git clone https://github.com/AngusJohnson/Clipper2.git ffi-packages\Clipper2\Clipper2 && cd ffi-packages\Clipper2\Clipper2
-git checkout e9bd409439bcb7a07a50f555c88e623986eea10f && cd ..\..\build
+git checkout e9bd409439bcb7a07a50f555c88e623986eea10f && cd ..\buildCP
 
-cmake .. && cmake --build . --config Release --parallel
+cmake .. && cmake --build . --config Release --parallel && cd ..\..\..\
+
+@rem TurboJPEG
+if not exist ffi-packages\Images\buildJP ( mkdir ffi-packages\Images\buildJP )
+
+git clone https://github.com/libjpeg-turbo/libjpeg-turbo.git ffi-packages\Images\libjpeg-turbo && cd ffi-packages\Images\libjpeg-turbo
+git checkout 8162eddf041e0be26f5c671bb6528723c55fed9d && cd ..\buildJP
+
+cmake ..\libjpeg-turbo && cmake --build . --config Release --parallel && cd ..\..\..\
--- ffi-packages/build.sh
+++ ffi-packages/build.sh
@@ -1,9 +1,35 @@
 #!/bin/sh
 
-[ ! -d ffi-packages/build ] && mkdir -p ffi-packages/build
+# Clipper2
+if [ "$1" == "-all" ]; then
+    [ ! -d ffi-packages/Clipper2/buildCP ] && mkdir -p ffi-packages/Clipper2/buildCP
 
-git clone https://github.com/AngusJohnson/Clipper2.git ffi-packages/Clipper2/Clipper2 && cd ffi-packages/Clipper2/Clipper2
-git checkout e9bd409439bcb7a07a50f555c88e623986eea10f
+    git clone https://github.com/AngusJohnson/Clipper2.git ffi-packages/Clipper2/Clipper2 && cd ffi-packages/Clipper2/Clipper2
+    git checkout e9bd409439bcb7a07a50f555c88e623986eea10f
 
-cmake ../../ -DCMAKE_CXX_COMPILER=g++ -G "Unix Makefiles" -B ../../build
-cd ../../build && make
+    cmake .. -DCMAKE_CXX_COMPILER=g++ -G "Unix Makefiles" -B ../buildCP
+    cd ../buildCP && make && cd ../../../
+fi
+
+# TurboJPEG
+if [ "$1" == "-all" ]; then
+    [ ! -d ffi-packages/Images/buildJP ] && mkdir -p ffi-packages/Images/buildJP
+
+    git clone https://github.com/libjpeg-turbo/libjpeg-turbo.git ffi-packages/Images/libjpeg-turbo && cd ffi-packages/Images/libjpeg-turbo
+    git checkout 8162eddf041e0be26f5c671bb6528723c55fed9d
+
+    cmake ../libjpeg-turbo -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -G "Unix Makefiles" -B ../buildJP
+    cd ../buildJP && make && cd ../../../
+fi
+
+# LodePNG and GifLib
+[ ! -d ffi-packages/Images/buildLG ] && mkdir -p ffi-packages/Images/buildLG
+
+git clone https://github.com/lvandeve/lodepng.git ffi-packages/Images/lodepng && cd ffi-packages/Images/lodepng
+git checkout 997936fd2b45842031e4180d73d7880e381cf33f && cp -f lodepng.cpp lodepng.c && cd ../../../
+
+git clone https://github.com/rcancro/giflib.git ffi-packages/Images/giflib && cd ffi-packages/Images/giflib
+git checkout 4b0c893cfddf16421bd3f59207fdf65f06e9a10d
+
+cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -G "Unix Makefiles" -B ../buildLG
+cd ../buildLG && make && cd ../../../
