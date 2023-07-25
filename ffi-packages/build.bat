3d70ab3373dd339f416dcc34c736c50fac3e1be3
Ruan Dias <rdkleriston@gmail.com>
2023-07-25 13:03:51 -0300

    Fix SHA

--- ffi-packages/build.bat
+++ ffi-packages/build.bat
@@ -12,6 +12,6 @@ cmake .. && cmake --build . --config Release --parallel && cd ..\..\..\
 if not exist ffi-packages\Images\buildJP ( mkdir ffi-packages\Images\buildJP )
 
 git clone https://github.com/libjpeg-turbo/libjpeg-turbo.git ffi-packages\Images\libjpeg-turbo && cd ffi-packages\Images\libjpeg-turbo
-git checkout 866e40201476b9af9fec5ce80de16bf0b03eff7a && cd ..\buildJP
+git checkout 8162eddf041e0be26f5c671bb6528723c55fed9d && cd ..\buildJP
 
 cmake ..\libjpeg-turbo && cmake --build . --config Release --parallel && cd ..\..\..\
